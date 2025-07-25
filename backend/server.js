f8162af59315fea747f9d635ae3a9e1b10030341
Leo Marques <marquessilva600@gmail.com>
2025-07-25 12:48:31 -0300

    Adiciona domínio do GitHub Pages às origens permitidas pelo CORS

--- backend/server.js
+++ backend/server.js
@@ -13,7 +13,8 @@ const app = express();
 const allowedOrigins = [
   'http://localhost:3000',
   'https://bibliotecabp.com.br',
-  'http://bibliotecabp.com.br'
+  'http://bibliotecabp.com.br',
+  'https://leomarquessilva.github.io'
 ];
 
 app.use(cors({
